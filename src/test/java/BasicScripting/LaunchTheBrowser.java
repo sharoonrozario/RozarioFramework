38bf4f9b60e78c192134576eed2644356ea93072
sharoonrozario <sharoonrozario@localhost>
2023-11-01 22:19:09 +0530

    Starting Framework

---
+++ src/test/java/BasicScripting/LaunchTheBrowser.java
@@ -0,0 +1,12 @@
+package BasicScripting;
+
+import org.junit.Test;
+import org.testng.Reporter;
+
+
+public class LaunchTheBrowser {
+	@Test
+	public  void run() {
+		Reporter.log("hello",true);
+	}
+}
